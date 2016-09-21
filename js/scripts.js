cc1cc7185f193f643657a522ed9e1d705244e987
howserstrousers <fritzcharleshowser@gmail.com>
2016-09-21 19:32:32 -0400

    play

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,4 @@
-/*
-$(document).ready(function(){
+/*$(document).ready(function(){
 
-	
-	$(".samnav").click(function(){
-	    var div = $(".homelogo");
-		    div.animate({left: '90%'});
-		    div.animate({up:'50%'});
-		    div.animate({right:'100%'});
-		    div.animate({down: '100%'});
-		    div.animate({left: '50%'});
-		    div.animate({up: '50%'});
- 
-    	});
-	});
-
-	
-	$(".homelogo").ready(function(){
-	    var div = $(".homelogo");
-	    div.animate({left: '90%'}, "slow");
-	    div.animate({right: '90%'}, "slow");
-	   
-		    div.animate({up:'50%'});
-		    div.animate({right:'100%'});
-		    div.animate({down: '100%'});
-		    div.animate({left: '50%'});
-		    div.animate({up: '50%'});
-		 
-	});
-
-});
- 
-
-$(document).ready(function() {
-    function loop() {
-        $('.homelogo').css({right:0});
-        $('.homelogo').animate ({
-            right: 100%,
-        }, 
-        5000, 'linear', function() {
-            loop();
-        });
-    }
-    loop();
 });
 */
