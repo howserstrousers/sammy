bade6a8ff86fa1298500f4b803b81c3973fbbe8d
howserstrousers <fritzcharleshowser@gmail.com>
2016-09-22 12:31:30 -0400

    some fixes

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,21 @@ $(document).ready(function() {
   $('#player').click(function(){
     player.play();
   });
+
+  $("#player").click(function(){
+    	$(".pauser").toggle();
+    	$("#player").toggle();
+    });
+
+
+
+  $('.pauser').click(function(){
+    player.pause();
+  });
+
+  $(".pauser").click(function(){
+    	$("#player").toggle();
+    	$(".pauser").toggle();
+    });
+
 });
