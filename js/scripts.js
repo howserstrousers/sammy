1493c69f59e01b851ca2e3efd4be4127d3414bd1
howserstrousers <fritzcharleshowser@gmail.com>
2016-09-27 21:20:03 -0400

    i

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,50 @@
-var player = new Audio("img/songs/test.mp3"); 
+window.onload = function() {
 
-$(document).ready(function() {
-  $('#player').click(function(){
-    player.play();
-  });
+  var player = new Audio("img/songs/test.mp3"); 
 
-  $("#player").click(function(){
-    	$(".pauser").toggle();
-    	$("#player").toggle();
+  $(document).ready(function() {
+    $('#player').click(function(){
+      player.play();
     });
 
+    $("#player").click(function(){
+      	$(".pauser").toggle();
+      	$("#player").toggle();
+      });
 
 
-  $('.pauser').click(function(){
-    player.pause();
-  });
 
-  $(".pauser").click(function(){
-    	$("#player").toggle();
-    	$(".pauser").toggle();
+    $('.pauser').click(function(){
+      player.pause();
     });
 
-});
+    $(".pauser").click(function(){
+      	$("#player").toggle();
+      	$(".pauser").toggle();
+      });
 
-if (window.DeviceOrientationEvent) {
-  console.log("DeviceOrientation is supported");
-  window.addEventListener('deviceorientation', function(eventData) {});
+  });
+
+  if (window.DeviceOrientationEvent) {
+    console.log("DeviceOrientation is supported");
+    window.addEventListener('deviceorientation', function(eventData) {});
+  }
+
+  var LR = eventData.gamma;
+  var FB = eventData.beta;
+  var DIR = eventData.alpha;
+
+  deviceOrientationHandler(LR, FB, DIR);
+   
+  function deviceOrientationHandler(LR, FB, DIR) {
+  //for webkit browser
+  document.getElementById("waves").style.webkitTransform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
+   
+  //for HTML5 standard-compliance
+  document.getElementById("waves").style.transform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
+  }
+
+
+
+
 }
-
-var LR = eventData.gamma;
-var FB = eventData.beta;
-var DIR = eventData.alpha;
-
-deviceOrientationHandler(LR, FB, DIR);
- 
-function deviceOrientationHandler(LR, FB, DIR) {
-//for webkit browser
-document.getElementById("waves").style.webkitTransform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
- 
-//for HTML5 standard-compliance
-document.getElementById("waves").style.transform = "rotate("+ LR +"deg) rotate3d(1,0,0, "+ (FB*-1)+"deg)";
-}
-
-
-
-
